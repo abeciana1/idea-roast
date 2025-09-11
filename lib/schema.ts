c7d061ea61e70e154bd14a1a447d3f822581d318
abeciana1 <alex.beciana@gmail.com>
2025-09-11 15:19:31 -0600

    IR-2 Adjusted schema

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
-export const inputSchema = z
+export const InputSchema = z.object({
+  prompt: z
   .string()
   .trim()
   .min(20, "Please add more details about your idea")
-  .max(5000, "Please keep your idea under 5000 characters");
+  .max(5000, "Please keep your idea under 5000 characters")
+})
 
-export type InputSchemaType = z.infer<typeof inputSchema>;
+export type InputSchemaType = z.infer<typeof InputSchema>;
