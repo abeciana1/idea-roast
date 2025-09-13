e9a72bee5ad917543c3c32c13747948d76055229
abeciana1 <alex.beciana@gmail.com>
2025-09-13 17:43:09 -0600

    IR-5 Added footer to render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import PromptForm from '@/components/_form/PromptForm'
+import Footer from '@/components/_general/Footer'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <PromptForm />
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      </main>
+      <Footer>
+        <PromptForm />
+      </Footer>
+    </>
   );
 }
