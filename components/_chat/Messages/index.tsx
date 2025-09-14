b3c21a20473ed5e67f9ab11833b67dfde619bdd7
abeciana1 <alex.beciana@gmail.com>
2025-09-14 17:13:10 -0600

    IR-5 Added styles for message component and container

--- components/_chat/Messages/index.tsx
+++ components/_chat/Messages/index.tsx
@@ -1,20 +1,49 @@
 import { UIMessage } from 'ai';
+import clsx from 'clsx';
 
 type MessageContainerProps = {
   messages: UIMessage[]
 }
 
+type RoleType = 'assistant' | 'user'
+
+type PartsType = {
+  type: 'text'
+  text: string
+}
+
+type MessageProps = {
+  role: RoleType
+  parts: PartsType[]
+}
+
 const MessageContainer: React.FC<MessageContainerProps> = ({ messages }) => {
   console.log('messages', messages)
   return (
-    <section className="relative space-y-6 mx-auto overflow-y-auto"></section>
+    <section className="relative space-y-6 mx-auto overflow-y-auto grid grid-cols-1 justify-items-stretch">
+      {messages.map((message, index) => (
+        <Message key={index} role={message.role as RoleType} parts={message.parts as PartsType[]} />
+      ))}
+    </section>
   )
 };
 
 export default MessageContainer;
 
-export const Message = () => {
+export const Message: React.FC<MessageProps> = ({
+  role,
+  parts
+}) => {
   return (
-    <div></div>
+    <div
+      className={clsx('text-lg px-3 py-1', {
+        ['justify-self-start bg-darkGrey text-foreground']: role === 'assistant',
+        ['justify-self-end bg-foreground text-background']: role === 'user',
+      })}
+    >{parts?.map((part) => {
+      if (part.type === 'text') {
+        return part.text
+      }
+    })}</div>
   )
 }
