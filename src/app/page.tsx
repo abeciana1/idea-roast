0ba03c74659e2697120877e7fe61a87a13a50992
abeciana1 <alex.beciana@gmail.com>
2025-09-14 15:46:48 -0600

    IR-5 Reordered comps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 import PromptForm from '@/components/_form/PromptForm'
-import Footer from '@/components/_general/Footer'
-import MessageContainer from '@/components/_chat/Messages'
 
 export default function Home() {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <MessageContainer/>
-      </main>
-      <Footer>
         <PromptForm />
-      </Footer>
+      </main>
     </>
   );
 }
