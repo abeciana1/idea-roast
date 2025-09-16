df7ff5e7d4dcef4cb383ad4a8153bcb723daa262
abeciana1 <alex.beciana@gmail.com>
2025-09-16 11:04:50 -0600

    IR-5 Formatted with prettier

--- components/_chat/Messages/index.tsx
+++ components/_chat/Messages/index.tsx
@@ -1,60 +1,59 @@
-import { UIMessage } from 'ai';
-import clsx from 'clsx';
+import { UIMessage } from "ai";
+import clsx from "clsx";
 
 type MessageContainerProps = {
-  messages: UIMessage[]
-}
+  messages: UIMessage[];
+};
 
-type RoleType = 'assistant' | 'user'
+type RoleType = "assistant" | "user";
 
 type PartsType = {
-  type: 'text'
-  text: string
-}
+  type: "text";
+  text: string;
+};
 
 type MessageProps = {
-  role: RoleType
-  parts: PartsType[]
-}
+  role: RoleType;
+  parts: PartsType[];
+};
 
 const MessageContainer: React.FC<MessageContainerProps> = ({ messages }) => {
-  console.log('messages', messages)
   return (
     <section className="relative w-full max-w-[1000px] space-y-6 mx-auto overflow-y-scroll grid grid-cols-1 justify-items-stretch mb-32">
       {messages.map((message, index) => (
-        <Message key={index} role={message.role as RoleType} parts={message.parts as PartsType[]} />
+        <Message
+          key={index}
+          role={message.role as RoleType}
+          parts={message.parts as PartsType[]}
+        />
       ))}
     </section>
-  )
+  );
 };
 
 export default MessageContainer;
 
-export const Message: React.FC<MessageProps> = ({
-  role,
-  parts
-}) => {
+export const Message: React.FC<MessageProps> = ({ role, parts }) => {
   return (
-    <div className=''>
-      <div
-        className={clsx('text-lg px-3 py-1 rounded-lg')}
-      >
+    <div className="">
+      <div className={clsx("text-lg px-3 py-1")}>
         {parts?.map((part, index) => {
-          if (part.type === 'text' && role === 'user') {
-            return part.text
-          } else if (part.type === 'text' && role === 'assistant') {
+          if (part.type === "text" && role === "user") {
+            return part.text;
+          } else if (part.type === "text" && role === "assistant") {
             return (
-              <span key={index} dangerouslySetInnerHTML={{ __html: part.text }} />
-            )
+              <span
+                key={index}
+                dangerouslySetInnerHTML={{ __html: part.text }}
+              />
+            );
           }
         })}
       </div>
-      <div
-        className='flex items-center justify-between text-sm text-darkGrey mt-1'
-      >
-        <div>{ role }</div>
+      <div className="flex items-center justify-between text-sm text-darkGrey mt-1">
+        <div className="capitalize">{role}</div>
         <div></div>
       </div>
     </div>
-  )
-}
+  );
+};
