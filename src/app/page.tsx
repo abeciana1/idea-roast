26e11fc8364ed820e92a91983178a682d78180f1
abeciana1 <alex.beciana@gmail.com>
2025-09-17 08:55:10 -0600

    Rendered Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import PromptForm from '@/components/_form/PromptForm'
+import Footer from '@/components/_general/Footer'
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <PromptForm />
       </main>
+      <Footer />
     </>
   );
 }
